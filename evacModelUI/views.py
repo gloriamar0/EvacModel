f3264cbf3fcb9da1986c2dac0454e4e0aa6ce8fd
gloriamar0 <gloriamar0@tamu.edu>
2023-10-11 12:43:51 -0500

    Successfully connected database

--- evacModelUI/views.py
+++ evacModelUI/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse #may not need it if not used
 from utils import get_db_handle
+from .models import test_collection
+
 
 def home(request):
     return render(request, "evacModelUI/home.html")
 
 def results(request):
+    cities = test_collection.find()
+    return HttpResponse(cities)
     return render(request, "evacModelUI/results.html")
