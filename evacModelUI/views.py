f8ac26df983fd34880a16e008fa4fde5fd6de2e8
gloriamar0 <gloriamar0@tamu.edu>
2023-10-14 23:44:49 -0500

    Added output files to database

--- evacModelUI/views.py
+++ evacModelUI/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse #may not need it if not used
 from utils import get_db_handle
+import gridfs
 
 def home(request):
     return render(request, "evacModelUI/home.html")
@@ -9,9 +10,45 @@ def results(request):
     # Loading in database and collections
     url = 'mongodb://localhost:27017/'
     db = get_db_handle("evacModelData", url)
-    input_collection = db['input']
+
+
+
+    # Storing output files into database
+    fs = gridfs.GridFS(db)  #used for large files
+    output_files = ['modestats_stackedbar.png',
+                    'modestats.png',
+                    'modestats.txt',
+                    'ph_modestats.png',
+                    'ph_modestats.txt',
+                    'pkm_modestats.png',
+                    'pkm_modestats.txt',
+                    'scorestats.png',
+                    'scorestats.txt',
+                    'stopwatch.png',
+                    'stopwatch.txt',
+                    'traveldistancestats.txt',
+                    'traveldistancestatslegs.png',
+                    'traveldistancestatstrips.png']
+    
+    # Writing in the database
+    # for file in output_files:
+    #     file_location = "evacModelUI/outputForTesting/" + file
+    #     file_data = open(file_location, "rb")
+    #     data = file_data.read()
+    #     file_data.close()
+
+    #     fs.put(data, filename = file)
+
+    data = db.fs.files.find_one({'filename': 'stopwatch.png'})
+    outputData = fs.get(data['_id']).read()
+    decoded = open('evacModelUI/static/evacModelUI/test5.png', 'wb')
+    decoded.write(outputData)
+    decoded.close()
+
+
 
     # Retrieving information from POST request and updating database
+    input_collection = db['input']
     if request.method == "POST":
         inputForm = request.POST
         inputCity = inputForm['cities']
