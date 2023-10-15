d51b4456aad2938729220b0c8e4c1601ac593800
gloriamar0 <gloriamar0@tamu.edu>
2023-10-14 19:04:51 -0500

    Able to write+update database

--- evacModelUI/views.py
+++ evacModelUI/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse #may not need it if not used
 from utils import get_db_handle
-#from .models import test_collection
-
 
 def home(request):
-    return render(request, "evacModelUI/home.html", {})
+    return render(request, "evacModelUI/home.html")
 
 def results(request):
+    # Loading in database and collections
     url = 'mongodb://localhost:27017/'
     db = get_db_handle("evacModelData", url)
-    cities_collection = db['cities']
-    #cities_collection.insertOne({'name' : 'Test'}) ## doesn't work because it is a GET request
-    cities = cities_collection.find()
-    return render(request, "evacModelUI/results.html", {'city_list' : cities})
+    input_collection = db['input']
+
+    # Retrieving information from POST request and updating database
+    if request.method == "POST":
+        inputForm = request.POST
+        inputCity = inputForm['cities']
+        inputEvent = inputForm['event']
+        inputPopulation = inputForm['population']
+        input_collection.update_one(
+            {'id': 0},
+            {'$set': {'city': inputCity,
+                      'event': inputEvent,
+                      'population': inputPopulation} })
+        # input_collection.insert_one(
+        #     {'id': 0,
+        #      'city': inputCity,
+        #      'event': inputEvent,
+        #      'population': inputPopulation} )
+
+    # Displaying results
+    inputs = input_collection.find()
+    return render(request, "evacModelUI/results.html", {'inputs_list': inputs })
