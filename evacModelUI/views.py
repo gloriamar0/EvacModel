dc922af64ad02d17a44bf8eff0f57107ded97a7e
gloriamar0 <gloriamar0@tamu.edu>
2023-11-13 09:38:51 -0600

    Succefully dockerizes (not including static files)

--- evacModelUI/views.py
+++ evacModelUI/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def results(request):
     # Loading in database and collections
-    url = 'mongodb://localhost:27017/'
+    url = 'mongodb+srv://gloriamar0:V6mSlQAbfbaSzzj1@cluster0.c0et8hr.mongodb.net/'
     db = get_db_handle("evacModelData", url)
 
     # Retrieving information from POST request and updating database
@@ -22,70 +22,70 @@ def results(request):
             {'id': 0},
             {'$set': {'city': inputCity,
                       'event': inputEvent,
-                      'population': inputPopulation} })
-        
-        """ input_collection.insert_one(
-            {'id': 0,
-             'city': inputCity,
-             'event': inputEvent,
-             'population': inputPopulation} ) """
+                      'population': inputPopulation} })  
+        # input_collection.insert_one(
+        #     {'id': 0,
+        #      'city': inputCity,
+        #      'event': inputEvent,
+        #      'population': inputPopulation} )
+
     inputs = input_collection.find()
 
     # Storing output files into database
-    fs = gridfs.GridFS(db)  #used for large files
-    output_files = ['modestats_stackedbar.png',
-                    'modestats.png',
-                    'modestats.txt',
-                    'ph_modestats.png',
-                    'ph_modestats.txt',
-                    'pkm_modestats.png',
-                    'pkm_modestats.txt',
-                    'scorestats.png',
-                    'scorestats.txt',
-                    'stopwatch.png',
-                    'stopwatch.txt',
-                    'traveldistancestats.txt',
-                    'traveldistancestatslegs.png',
-                    'traveldistancestatstrips.png']
+    # fs = gridfs.GridFS(db)  #used for large files
+    # output_files = ['modestats_stackedbar.png',
+    #                 'modestats.png',
+    #                 'modestats.txt',
+    #                 'ph_modestats.png',
+    #                 'ph_modestats.txt',
+    #                 'pkm_modestats.png',
+    #                 'pkm_modestats.txt',
+    #                 'scorestats.png',
+    #                 'scorestats.txt',
+    #                 'stopwatch.png',
+    #                 'stopwatch.txt',
+    #                 'traveldistancestats.txt',
+    #                 'traveldistancestatslegs.png',
+    #                 'traveldistancestatstrips.png']
     
-    ## Writing in the database
+    # # Writing in the database
     # for file in output_files:
     #     file_location = "evacModelUI/outputForTesting/" + file
     #     file_data = open(file_location, "rb")
     #     data = file_data.read()
     #     file_data.close()
-
+   
     #     fs.put(data, filename = file)
     
-    # Deciding which files to display in UI
-    display_images = ['scorestats.png',
-                      'stopwatch.png']
+    # # Deciding which files to display in UI
+    # display_images = ['scorestats.png',
+    #                   'stopwatch.png']
                      
-    display_text = ['scorestats.txt']
+    # display_text = ['scorestats.txt']
 
-    # Retrieving PNGs and saving in static folder
-    for img_file_name in display_images:
-        data = db.fs.files.find_one({'filename': img_file_name})
-        file_content = fs.get(data['_id']).read()
-        output_file = open("evacModelUI/static/evacModelUI/" + img_file_name, 'wb')
-        output_file.write(file_content)
-        output_file.close()
+    # # Retrieving PNGs and saving in static folder
+    # for img_file_name in display_images:
+    #     data = db.fs.files.find_one({'filename': img_file_name})
+    #     file_content = fs.get(data['_id']).read()
+    #     output_file = open("evacModelUI/static/evacModelUI/" + img_file_name, 'wb')
+    #     output_file.write(file_content)
+    #     output_file.close()
 
-    # Retrieving TXT files
+    # # # Retrieving TXT files
     txt_file_content = {}
-    for txt_file_name in display_text:
-        data = db.fs.files.find_one({'filename': txt_file_name})
-        file_content = fs.get(data['_id']).readlines()
+    # for txt_file_name in display_text:
+    #     data = db.fs.files.find_one({'filename': txt_file_name})
+    #     file_content = fs.get(data['_id']).readlines()
 
-        headers = file_content.pop(0).decode().split("\t")
-        content = []
-        for line in file_content:
-            content.append(line.decode().split("\t"))
-            for i in range(1, len(headers)):
-                content[-1][i] = content[-1][i][0:6]
+    #     headers = file_content.pop(0).decode().split("\t")
+    #     content = []
+    #     for line in file_content:
+    #         content.append(line.decode().split("\t"))
+    #         for i in range(1, len(headers)):
+    #             content[-1][i] = content[-1][i][0:6]
 
-        txt_file_content.update({'title': 'Score Statistics Table:',
-                                 'headers': headers,
-                                 'content': content})
+    #     txt_file_content.update({'title': 'Score Statistics Table:',
+    #                              'headers': headers,
+    #                              'content': content})
 
     return render(request, "evacModelUI/results.html", {'inputs_list': inputs, 'text_list': txt_file_content})
