7b8ab1cdff8c3de27b17471b19995b445b6fc4ae
gloriamar0 <gloriamar0@tamu.edu>
2023-11-28 01:47:37 -0600

    Cleaning up code (part 2)

--- evacModelUI/db_functions.py
+++ evacModelUI/db_functions.py
@@ -12,6 +12,7 @@ def insertInputs(input_collection, inputForm):
                      'event': inputEvent,
                      'population': inputPopulation} )
 
+# Updates inputs from exisiting database entry
 def updateInputs(input_collection, inputForm):
     # Retrieving each input
     inputCity = inputForm['cities']
@@ -29,6 +30,7 @@ def storeLargeFileDatabase(fs, file_name, file_location):
     file_data.close()
     fs.put(data, filename = file_name)
 
+# Retrives images in an encoded Base64 format
 def getImages(db, fs, img_name_list):
     images = []
     for img_name in img_name_list:
@@ -36,3 +38,21 @@ def getImages(db, fs, img_name_list):
         file_content = b64encode(fs.get(data['_id']).read())
         images.append(str(file_content, 'utf-8'))
     return images
+
+# Retrives text files formatted ready for implementation in a table
+def getTextFiles(db, fs, txt_name_list):
+    txt_files = {}  #empty dictionary
+    for txt_name in txt_name_list:
+        data = db.fs.files.find_one({'filename': txt_name})
+        file_content = fs.get(data['_id']).readlines()
+
+        headers = file_content.pop(0).decode().split("\t")
+        content = []
+        for line in file_content:
+            content.append(line.decode().split("\t"))
+            for i in range(1, len(headers)):
+                content[-1][i] = content[-1][i][0:6]
+
+        txt_files.update({'title': 'Score Statistics Table:', 'headers': headers, 'content': content})
+        
+    return txt_files
--- evacModelUI/views.py
+++ evacModelUI/views.py
@@ -39,35 +39,11 @@ def results(request):
     
     # Deciding which files to display in UI
     images_list = ['scorestats.png','stopwatch.png']
-    display_text = ['scorestats.txt']
+    text_files_list = ['scorestats.txt']
 
-    # Retrieving PNGs and saving in static folder
-    # for img_file_name in images_list:
-    #     data = db.fs.files.find_one({'filename': img_file_name})
-    #     file_content = fs.get(data['_id']).read()
-    #     output_file = open("evacModelUI/static/evacModelUI/" + img_file_name, 'wb')
-    #     output_file.write(file_content)
-    #     output_file.close()
-
-    # Retrieving TXT files
-    txt_file_content = {}
-    for txt_file_name in display_text:
-        data = db.fs.files.find_one({'filename': txt_file_name})
-        file_content = fs.get(data['_id']).readlines()
-
-        headers = file_content.pop(0).decode().split("\t")
-        content = []
-        for line in file_content:
-            content.append(line.decode().split("\t"))
-            for i in range(1, len(headers)):
-                content[-1][i] = content[-1][i][0:6]
-
-        txt_file_content.update({'title': 'Score Statistics Table:',
-                                 'headers': headers,
-                                 'content': content})
-        
     # Retrieving data for display
     inputs = input_collection.find()
     images = getImages(db, fs, images_list)
+    txt_file_content = getTextFiles(db, fs, text_files_list)
 
     return render(request, "evacModelUI/results.html", {'inputs_list': inputs, 'images': images, 'text_list': txt_file_content})
